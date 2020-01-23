e73e4a09e19594bd0ff7996ad2428fc6dddd55ca
Dr. Njitram <martijn.gobes@gmail.com>
2020-01-23 16:32:42 +0100

    uni work

--- airflow.m
+++ airflow.m
@@ -1,87 +1,85 @@
-close all;
-clear;
-clc;
-format short;
+function results = airflow(height, source_str, showgraph)
+    % physical parameters
+    % source http://www.conditionaire.com.au/school-type.html
+    dynamic_visc = 1.825E-5; % dynamic viscocity of air at 20C
+    length_hood = 0.9; % height of entire hood
+    size_opening = 0.3; % size of vent
+    %height = 0.1; % 0.1 - 0.65 height of opening
+    width = 0.695; % depth of hood
+    hood_thick = 0.025; % thickness of hood
+    blockage_thick = 0.01; % thickness of the blockage
+    blockage_offset = [0.638 0.250]; % x y offset of the blockage
 
-% physical parameters
-% source http://www.conditionaire.com.au/school-type.html
-dynamic_visc = 1.825E-5; % dynamic viscocity of air at 20C
-length_hood = 0.9; % height of entire hood
-size_opening = 0.3; % size of vent
-height = 0.1; % 0.1 - 0.65 height of opening
-width = 0.695; % depth of hood
-hood_thick = 0.025; % thickness of hood
-blockage_thick = 0.01; % thickness of the blockage
-blockage_offset = [0.638 0.250]; % x y offset of the blockage
+    vent_strength = 200; % 200-317 L/s
+    source_strength = source_str; % strength, roughly 0.025 for testing
 
-vent_strength = 200; % 200-317 L/s
-source_strength = 0.025;
+    % Model Parameters
+    max_mesh = 0.01;
 
-% Model Parameters
-max_mesh = 0.01;
+    % create a model container
+    model = createpde(1);
 
-% create a model container
-model = createpde();
+    % set geometry (see documentation)
+    fume = [2; 10;... % Fume hood
+        0; width; width;       width - 0.057; width - 0.057;      width - size_opening - 0.057;width - size_opening - 0.057; hood_thick;  hood_thick;  0;...
+        0; 0;     length_hood; length_hood;   length_hood + 0.05; length_hood+0.05;length_hood;                  length_hood;      height;       height];
 
-% set geometry (see documentation)
-fume = [2; 10;... % Fume hood
-    0; width; width;       width - 0.057; width - 0.057;      width - size_opening - 0.057;width - size_opening - 0.057; hood_thick;  hood_thick;  0;...
-    0; 0;     length_hood; length_hood;   length_hood + 0.05; length_hood+0.05;length_hood;                  length_hood;      height;       height];
-        
-source = [3; 4;... % source
-    width * 0.8; width * 0.8 + 0.01; width * 0.8 + 0.01; width * 0.8;...
-    0;           0;                  0.08;               0.08;];
+    source = [3; 4;... % source
+        width * 0.8; width * 0.8 + 0.01; width * 0.8 + 0.01; width * 0.8;...
+        0;           0;                  0.08;               0.08;];
 
-blocking = [2; 6;... % blockage
-    blockage_offset(1); blockage_offset(1) + blockage_thick; blockage_offset(1) + blockage_thick - 0.143; blockage_offset(1) + blockage_thick * 2.5 - 0.343; blockage_offset(1) - 0.321; blockage_offset(1) - 0.143;...
-    blockage_offset(2); blockage_offset(2)                 ; blockage_offset(2) + 0.558                 ; blockage_offset(2) + 0.605                       ; blockage_offset(2) + 0.595; blockage_offset(2) + 0.55;];
+    blocking = [2; 6;... % blockage
+        blockage_offset(1); blockage_offset(1) + blockage_thick; blockage_offset(1) + blockage_thick - 0.143; blockage_offset(1) + blockage_thick * 2.5 - 0.343; blockage_offset(1) - 0.321; blockage_offset(1) - 0.143;...
+        blockage_offset(2); blockage_offset(2)                 ; blockage_offset(2) + 0.558                 ; blockage_offset(2) + 0.605                       ; blockage_offset(2) + 0.595; blockage_offset(2) + 0.55;];
 
-outside = [3; 4;...
-    -1; 0; 0; -1;...
-    -0.5; -0.5; 1.2; 1.2;];
+    outside = [3; 4;...
+        -1; 0; 0; -1;...
+        -0.5; -0.5; 1.2; 1.2;];
 
-source = [source; zeros(length(fume)-length(source),1)];
-blocking = [blocking; zeros(length(fume)-length(blocking),1)];
-outside = [outside; zeros(length(fume)-length(outside),1)];
+    source = [source; zeros(length(fume)-length(source),1)];
+    blocking = [blocking; zeros(length(fume)-length(blocking),1)];
+    outside = [outside; zeros(length(fume)-length(outside),1)];
 
-gd = [fume source blocking outside];
-ns = char('P1', 'SQ1', 'P2', 'SQ2')';
-sf = 'P1 - SQ1 - P2 + SQ2';
+    gd = [fume source blocking outside];
+    ns = char('P1', 'SQ1', 'P2', 'SQ2')';
+    sf = 'P1 - SQ1 - P2 + SQ2';
 
 
-g = decsg(gd, sf, ns);
-geometryFromEdges(model, g);
+    g = decsg(gd, sf, ns);
+    geometryFromEdges(model, g);
 
 
-% Generate Mesh
-generateMesh(model, 'Hmax', max_mesh);
+    % Generate Mesh
+    generateMesh(model, 'Hmax', max_mesh);
 
-%pdeplot(model)
-%pdegplot(model, 'EdgeLabels', 'on');
+    %pdeplot(model)
+    %pdegplot(model, 'EdgeLabels', 'on');
 
-% Boundary Conditions
-applyBoundaryCondition(model,'neumann','Edge', 1:25,'q',0,'g',0); % static walls
-applyBoundaryCondition(model,'dirichlet','Edge', 1, 'h', 1, 'r', vent_strength); % vent
-applyBoundaryCondition(model,'dirichlet','Edge', 6, 'h', 1, 'r', source_strength); % source
-applyBoundaryCondition(model,'dirichlet','Edge', [15 14], 'h', 1, 'r', 1); % opening
+    % Boundary Conditions
+    applyBoundaryCondition(model,'neumann','Edge', 1:25,'q',0,'g',0); % static walls
+    applyBoundaryCondition(model,'dirichlet','Edge', 1, 'h', 1, 'r', vent_strength); % vent
+    applyBoundaryCondition(model,'dirichlet','Edge', 6, 'h', 1, 'r', source_strength); % source
+    applyBoundaryCondition(model,'dirichlet','Edge', [15 14], 'h', 1, 'r', 1); % opening
 
-% Specify model parameters
-specifyCoefficients(model,'m',0,'d', 0,'c', -dynamic_visc ,'a',0,'f',0);
-setInitialConditions(model, 1);
+    % Specify model parameters
+    specifyCoefficients(model,'m',0,'d', 0,'c', -dynamic_visc ,'a',0,'f',0);
+    setInitialConditions(model, 1);
 
-% solve
-results = solvepde(model);
-
-% show results
-figure;
-ux = results.XGradients;
-uy = results.YGradients;
-pdeplot(model,'XYData',results.NodalSolution, 'FlowData', [ux uy], 'Contour', 'on');
-xlabel("x (m)");
-ylabel('y (m)');
-c = colorbar;
-c.Label.String = 'air concentration (???)';
-
+    % solve
+    results = solvepde(model);
+    
+    if showgraph
+        % show results
+        figure;
+        ux = results.XGradients;
+        uy = results.YGradients;
+        pdeplot(model,'XYData',results.NodalSolution, 'FlowData', [ux uy], 'Contour', 'on');
+        xlabel("x (m)");
+        ylabel('y (m)');
+        c = colorbar;
+        c.Label.String = 'air concentration (???)';
+    end
+end
 
 
 % Use [vx, vy] =  evaluateGradient(results,X,Y) to get gradient values
--- projecttube.m
+++ projecttube.m
@@ -3,33 +3,40 @@ clear;
 clc;
 format short;
 
+
 %% physical parameters
 length_hood = 0.9; % height of entire hood
-size_opening = 0.3; % size of vent
-height = 0.1; % 0.1 - 0.65 height of opening
 width = 0.695; % depth of hood
-hood_thick = 0.025; % thickness of hood
+size_opening = 0.3; % size of vent
+
+height = 0.1; % 0.1 - 0.65 height of sash
+hood_thick = 0.025; % thickness of sash
 
 blockage_thick = 0.01; % thickness of the blockage
 blockage_offset = [0.638 0.250]; % x y offset of the blockage
 
 
-width_source = 0.01;
-diffusion = 10 * 0.000176; % m2/s (oxygen in air)
-density = 1.2041; % air at room temperature
-production_s = 0.1; % production of oxygen (L/s)
+width_source = 0.01; % width of source
+speed_s = 0.1; % speed of oxygen (m/s)
+source_pos = [0.556 0.08]; % x y pos of source
 
-% combined value must not exceed 0.215
-vx = 0.0; % air speed in x in m/s
-vy = 0.0; % air speed in y in m/s
+air_flow_speed_correction = 1000; % to ensure air flow speed is within acceptable range, inverse (0.215 m/s)
+
+% natural constants
+diffusion = 1 * 0.000176; % m2/s (oxygen in air)
+density = 1.2041; % air at room temperature
+
 
 %% Model Parameters
-max_mesh = 0.01; % maximum mesh size ( scaled by x/length)
+max_mesh = 0.05; % maximum mesh size ( scaled by x/length)0.01 for good results, but slow, 0.05 for fast testing
 sim_length = 50; % duration (s)
 sim_steps = 100; % time steps
+trip_gas = 0.001; % amount of gas (m3) at entrance before gas has escaped
+
 
 %% create a model container
-model = createpde();
+model = createpde(1);
+
 
 %% set geometry
 fume = [2; 10;... % Fume hood
@@ -37,79 +44,111 @@ fume = [2; 10;... % Fume hood
     0; 0;     length_hood; length_hood;   length_hood + 0.05; length_hood+0.05;length_hood;                  length_hood;      height;       height];
         
 source = [3; 4;... % source
-    width * 0.8; width * 0.8 + width_source; width * 0.8 + width_source; width * 0.8;...
-    0;           0;                  0.08;               0.08;];
+    source_pos(1) - width_source/2; source_pos(1) + width_source/2; source_pos(1) + width_source/2; source_pos(1) - width_source/2;...
+    0;                              0;                              source_pos(2);                  source_pos(2);];
 
 blocking = [2; 6;... % blockage
     blockage_offset(1); blockage_offset(1) + blockage_thick; blockage_offset(1) + blockage_thick - 0.143; blockage_offset(1) + blockage_thick * 2.5 - 0.343; blockage_offset(1) - 0.321; blockage_offset(1) - 0.143;...
     blockage_offset(2); blockage_offset(2)                 ; blockage_offset(2) + 0.558                 ; blockage_offset(2) + 0.605                       ; blockage_offset(2) + 0.595; blockage_offset(2) + 0.55;];
 
-
 source = [source; zeros(length(fume)-length(source),1)];
 blocking = [blocking; zeros(length(fume)-length(blocking),1)];
-
 
 gd = [fume source blocking];
 ns = char('P1', 'SQ1', 'P2')';
 sf = 'P1 - SQ1 - P2';
 
-
 g = decsg(gd, sf, ns);
 geometryFromEdges(model, g);
 
+
 %% Generate Mesh
 generateMesh(model, 'Hmax', max_mesh);
-%pdegplot(model, 'EdgeLabels', 'on');
+
+
+%% Retrieve flow model
+flowresults = airflow(height, speed_s, true); % retrieve flow model with hood at a certain height and show the results in a graph
+
 
 %% Boundary Conditions
-source_strength = production_s / width_source; 
-applyBoundaryCondition(model,'neumann','Edge', 1:20 ,'q',0,'g',0); % static walls
-applyBoundaryCondition(model,'neumann','Edge', [1 5] ,'q',vx,'g',0); % 'open' walls
+source_strength = speed_s / width_source; 
+edge_cond = @(location, state) sum(evaluateGradient(flowresults, [location.x; location.y])'/air_flow_speed_correction);
+
+applyBoundaryCondition(model,'neumann','Edge', 1:20 ,'q', 0,'g', 0); % static walls
+applyBoundaryCondition(model,'neumann','Edge', [1 5] ,'q', edge_cond,'g', 0); % 'open' walls
 applyBoundaryCondition(model,'dirichlet','Edge', 7, 'h', 1, 'r', source_strength); % source edge
 
 
 %% set coefficients in PDE
-% use location.x/y/x and state.u/ux/uy/uz
-f =  @(location, state) -1*( vx*state.ux + vy*state.uy);
+f = @(location, state) sum(evaluateGradient(flowresults, [location.x; location.y])'/air_flow_speed_correction .* [state.ux; state.uy] * -1); % implement spread of gas based on flow model
 
-specifyCoefficients(model, 'm', 0, 'd', density, 'c', diffusion, 'a', 0, 'f', f); % outer pipe
+specifyCoefficients(model, 'm', 0, 'd', density, 'c', diffusion, 'a', 0, 'f', f); % Coefficients for model
 
-setInitialConditions(model, 0);
+setInitialConditions(model, 0); % Initial condition is no gas anywhere
+
 
 %% solve
 tlist = linspace(0, sim_length, sim_steps);
 results = solvepde(model, tlist);
 
+
+%% determine when front is reached
+y = linspace(0, height, 101);
+x = zeros(1, length(y));
+
+exit_time = 0;
+for i = 1:sim_steps
+    entrance_values = interpolateSolution(results, x, y, i); % measure values at entrance
+    volume = trapz(y, entrance_values); % integrate amount over entrance
+    if volume > trip_gas % compare check because float maths and i dont trust matlab
+        exit_time = i;
+        break
+    end
+end
+
+if exit_time ~= 0 % the gas escapes
+    figure;
+    plot(y, entrance_values);
+    title(['Gas exited at t=', num2str(sim_length * exit_time/sim_steps), 's']);
+else % the gas never escapes
+    disp(['The gas never escapes within the given time range of ', num2str(sim_length), 's']);
+    exit_time = sim_steps;
+end
+    
+
 %% show results
 figure;
-% subplot(3, 1, 1);
-% t = 1;
-% ux = results.XGradients(:, t);
-% uy = results.YGradients(:, t);
-% pdeplot(model,'XYData',results.NodalSolution(:, t), 'Contour', 'on','ColorMap','hot');
-% title(['Spread of gas at t=', num2str(sim_length * (t - 1)/sim_steps), 's']);
-% xlabel("x (m)");
-% ylabel('y (m)');
-% c = colorbar;
-% c.Label.String = 'gas concentration (???)';
-% 
-% subplot(3, 1, 2);
-% t = round(sim_steps/2);
-% ux = results.XGradients(:, t);
-% uy = results.YGradients(:, t);
-% pdeplot(model,'XYData',results.NodalSolution(:, t), 'Contour', 'on','ColorMap','hot');
-% title(['Spread of gas at t=', num2str(sim_length * t/sim_steps), 's']);
-% xlabel("x (m)");
-% ylabel('y (m)');
-% c = colorbar;
-% c.Label.String = 'gas concentration (???)';
-% 
-% subplot(3, 1, 3);
-t = sim_steps;
-ux = results.XGradients(:, t);
-uy = results.YGradients(:, t);
+subplot(3, 1, 1);
+t = 1;
+pdeplot(model,'XYData',results.NodalSolution(:, t), 'Contour', 'on','ColorMap','hot');
+title(['Spread of gas at t=', num2str(sim_length * (t - 1)/sim_steps), 's']);
+xlabel("x (m)");
+ylabel('y (m)');
+c = colorbar;
+c.Label.String = 'gas concentration (???)';
+
+subplot(3, 1, 2);
+t = round(sim_steps/2);
 pdeplot(model,'XYData',results.NodalSolution(:, t), 'Contour', 'on','ColorMap','hot');
 title(['Spread of gas at t=', num2str(sim_length * t/sim_steps), 's']);
+xlabel("x (m)");
+ylabel('y (m)');
+c = colorbar;
+c.Label.String = 'gas concentration (???)';
+
+subplot(3, 1, 3);
+t = sim_steps;
+pdeplot(model,'XYData',results.NodalSolution(:, t), 'Contour', 'on','ColorMap','hot');
+title(['Spread of gas at t=', num2str(sim_length * t/sim_steps), 's']);
+xlabel("x (m)");
+ylabel('y (m)');
+c = colorbar;
+c.Label.String = 'gas concentration (???)';
+
+%% show results at the exit time
+figure;
+pdeplot(model,'XYData',results.NodalSolution(:, exit_time), 'Contour', 'on','ColorMap','hot');
+title(['Spread of gas at t=', num2str(sim_length * exit_time/sim_steps), 's']);
 xlabel("x (m)");
 ylabel('y (m)');
 c = colorbar;
